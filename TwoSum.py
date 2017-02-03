26ed1402104675aad019233f94c5e053ba7fc890
吴强 <wuqiangroy@live.com>
2017-02-03 14:58:14 +0800

    Add files via upload

--- TwoSum.py
+++ TwoSum.py
@@ -13,7 +13,6 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
-import copy
 # 1
 
 
@@ -28,39 +27,33 @@ class Solution(object):
         for n1 in range(len(self.lst)-1):
             for n2 in range(n1+1, len(self.lst)):
                 if self.lst[n1] + self.lst[n2] == self.target:
-                    return [n1, n2]
+                    return [n1+1, n2+1]
+
 # 2
 
 
-class Solution2(object):
-
-    def __init__(self, lst, target):
-        self.lst = list(lst)
-        self.lst.sort()
-        self.target = int(target)
-
-    def TwoSum(self):
-        n = 0
-        while n < len(self.lst):
-            new_lst = copy.copy(self.lst)
-            new_lst.pop(n)
-            num = self.target - self.lst[n]
-            if num in new_lst:
-                return [n, new_lst.index(num)+1]
-                # 确定后者肯定大于前者，因为n是从第0位开始找，没找到才会越来越大
-            n += 1
+def twoSum(numbers, target):
+    # write your code here
+    n = 0
+    while n < len(numbers):
+        new_lst = [x for x in numbers]
+        new_lst.pop(n)
+        num = target - numbers[n]
+        if num in new_lst:
+            return [n + 1, new_lst.index(num) + 2]
+        n += 1
 
 if __name__ == '__main__':
     from timeit import Timer
     lst = [1, 3, 5, 6, 7, 8, 9]
     target = 11
+    lst2 = [1, 0, -1]
+    target2 = -1
     s = Solution(lst, target)
     print s.TwoSum()
-    s2 = Solution2(lst, target)
-    print s2.TwoSum()
+    print twoSum(lst2, target2)
 
     t1 = Timer(lambda: s.TwoSum())
-    t2 = Timer(lambda: s2.TwoSum())
+    t2 = Timer(lambda: twoSum(lst2, target2))
     print t1.timeit(1000000)
     print t2.timeit(1000000)
-
